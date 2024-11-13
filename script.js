e7d3f0c12f6246011e7e0f040a0933b39431aad8
JAYARAMAN K <150779617+jayaraman2212066@users.noreply.github.com>
2024-11-13 21:59:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function speakText(text) {
 
 async function getCo2Prediction(co2Data) {
     try {
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("http://127.0.0.1:10000/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
