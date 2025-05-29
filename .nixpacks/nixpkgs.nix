055494c9163671fd485f7dcbb7ee2f8e8246fffb
jayaraman2212066 <jayaraman2212066@ssn.edu.in>
2025-05-29 20:36:00 +0530

    fix: update nixpkgs.nix with pinned version and PostgreSQL 15

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -1,16 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz") {} }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python39
     python39Packages.pip
     python39Packages.virtualenv
-    postgresql_16
+    postgresql_15
     gcc
   ];
 
   shellHook = ''
     export PYTHONPATH="${pkgs.python39}/lib/python3.9/site-packages:$PYTHONPATH"
-    export LD_LIBRARY_PATH="${pkgs.postgresql_16}/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.postgresql_15}/lib:$LD_LIBRARY_PATH"
   '';
 } 
