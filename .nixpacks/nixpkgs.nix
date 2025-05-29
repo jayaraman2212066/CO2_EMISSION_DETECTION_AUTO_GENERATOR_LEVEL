073dd551157bd5ac6f9e08d40483447ad84ce4af
jayaraman2212066 <jayaraman2212066@ssn.edu.in>
2025-05-29 20:29:51 +0530

    fix: simplify nixpkgs.nix configuration for better compatibility

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -6,15 +6,11 @@ pkgs.mkShell {
     python39Packages.pip
     python39Packages.virtualenv
     postgresql_16
-    postgresql_16.lib
     gcc
-    gcc-unwrapped
-    gcc-unwrapped.lib
   ];
 
   shellHook = ''
     export PYTHONPATH="${pkgs.python39}/lib/python3.9/site-packages:$PYTHONPATH"
-    export LD_LIBRARY_PATH="${pkgs.postgresql_16}/lib:${pkgs.gcc-unwrapped.lib}/lib:$LD_LIBRARY_PATH"
-    export PATH="${pkgs.postgresql_16}/bin:$PATH"
+    export LD_LIBRARY_PATH="${pkgs.postgresql_16}/lib:$LD_LIBRARY_PATH"
   '';
 } 
