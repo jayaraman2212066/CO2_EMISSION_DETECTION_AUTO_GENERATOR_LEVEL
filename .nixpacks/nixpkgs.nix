9fd2c1274b80b513ef332dff25efdd71d4724262
jayaraman2212066 <jayaraman2212066@ssn.edu.in>
2025-05-29 20:29:00 +0530

    fix: enhance nixpkgs.nix with complete GCC and PostgreSQL configuration

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -8,10 +8,13 @@ pkgs.mkShell {
     postgresql_16
     postgresql_16.lib
     gcc
+    gcc-unwrapped
+    gcc-unwrapped.lib
   ];
 
   shellHook = ''
     export PYTHONPATH="${pkgs.python39}/lib/python3.9/site-packages:$PYTHONPATH"
-    export LD_LIBRARY_PATH="${pkgs.postgresql_16}/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.postgresql_16}/lib:${pkgs.gcc-unwrapped.lib}/lib:$LD_LIBRARY_PATH"
+    export PATH="${pkgs.postgresql_16}/bin:$PATH"
   '';
 } 
